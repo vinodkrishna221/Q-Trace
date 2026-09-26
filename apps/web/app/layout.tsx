e49ccecdda235b863143aeebc44c468cbe7d0629
vinodkrishna221 <ancemodernfacts@gmail.com>
2026-09-26 20:25:49 +0530

    style(fonts): switch typography stack to Geist and Geist Mono

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700&family=Geist+Mono:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
@@ -31,3 +31,4 @@ export default function RootLayout({
     </html>
   );
 }
+
